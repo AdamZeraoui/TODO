de384f7bc0d6dec9fe6dc5b2ea4e3014d60157b7
AdamZeraoui <adam.zeroaui@hotmail.fr>
2024-02-27 10:53:17 +0100

    End Todo

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let count = 1;
+
+
+const delBtn = document.querySelector('.delBtn');
+delBtn.addEventListener('click',function(){
+    deleteTask(taskCard);
+});
+const taskCard = document.querySelector(".todoCard");
+const tasksContainer = document.querySelector("#todoCards");
+const addBtn = document.querySelector('#btn'); //Allows you to select class and id in the CSS.
+addBtn.addEventListener('click', addTask); //add a task on click.
+
+function addTask(){
+    const newTask = taskCard.cloneNode(true); //clone the task card.
+    const newDelBtn = newTask.querySelector('.delBtn');
+    const newTextArea = newTask.querySelector(".task");
+ 
+    newTextArea.value = "New Task";
+    newDelBtn.addEventListener('click', function(){
+        deleteTask(newTask);
+        updateCount()
+    })
+
+    tasksContainer.appendChild(newTask);// append new task yo the tasks container
+    updateCount();
+
+}
+
+function deleteTask(task){
+    task.remove();
+}
+
+function updateCount(){
+    let newCount = document.querySelector ("#count");
+
+    newCount.textContent = 'Compteur('+ tasksContainer.children.length+')';
+}``
